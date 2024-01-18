2cdf3945a3be9e8db28fe5a314d8704713f4a7b8
Toni Fernandes <tonifernades124@gmail.com>
2024-01-18 14:30:32 +0000

    Saved classes on a different file

---
+++ classes.py
@@ -0,0 +1,104 @@
+import numpy as np
+
+class layer_dense:
+    def __init__(self, n_inputs, n_neurons, custom_weights=None, custom_biases=None):
+        if custom_weights is None:
+            self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+            self.biases = np.zeros((1, n_neurons))
+        else:
+            self.weights = custom_weights
+            self.biases = custom_biases
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs
+
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weights.T)
+
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+        self.inputs = inputs
+    
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
+
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+        self.inputs = inputs
+    
+    def backward(self, dvalues):
+        self.dinputs = np.empty_like(dvalues)
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            single_output = single_output.reshape(-1, 1)
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
+
+class Loss:
+    def calculate(self, output, y): # y is the target values
+        sample_losses = self.forward(output, y) 
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7) # clip both sides to prevent division by 0
+        
+        if len(y_true.shape) == 1: # if targets are in a single dimension
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        
+        elif len(y_true.shape) == 2: # if targets are in a one-hot encoded format
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+        
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        labels = len(dvalues[0])
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+        self.dinputs = -y_true / dvalues
+        self.dinputs = self.dinputs / samples
+
+
+class Activation_Softmax_Loss_CategoricalCrossentropy():
+    def __init__(self):
+        self.activation = Activation_Softmax()
+        self.loss = Loss_CategoricalCrossentropy()
+    
+    def forward(self, inputs, y_true):
+        self.activation.forward(inputs)
+        self.output = self.activation.output
+        return self.loss.calculate(self.output, y_true)
+    
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+        self.dinputs = dvalues.copy()
+        self.dinputs[range(samples), y_true] -= 1
+        self.dinputs = self.dinputs / samples
+
+
+class Optimizer_SGD:
+    global learning_rate
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+    def update_params(self, layer):
+        layer.weights += -self.learning_rate * layer.dweights
+        layer.biases += -self.learning_rate * layer.dbiases
+    def update_learning_rate(self, new_learning_rate):
+        self.learning_rate = new_learning_rate
--- train_neural_network.py
+++ train_neural_network.py
@@ -2,7 +2,7 @@ import datetime
 import numpy as np
 import json
 import os
-import shutil
+import classes
 
 training_inputs = []
 training_targets = []
@@ -16,6 +16,7 @@ learning_rate = None
 number_of_epochs = None
 loss = None
 best_loss = float('inf')
+rate_of_decrease = None
 
 # Weights and biases variables
 weights_layer1 = None
@@ -32,113 +33,6 @@ hidden_layer3 = None
 activation1 = None
 loss_activation = None
 optimizer = None
-
-
-np.random.seed(0)
-class layer_dense:
-    def __init__(self, n_inputs, n_neurons, custom_weights=None, custom_biases=None):
-        if custom_weights is None:
-            self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
-            self.biases = np.zeros((1, n_neurons))
-        else:
-            self.weights = custom_weights
-            self.biases = custom_biases
-
-    def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
-        self.inputs = inputs
-
-    def backward(self, dvalues):
-        self.dweights = np.dot(self.inputs.T, dvalues)
-        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
-        self.dinputs = np.dot(dvalues, self.weights.T)
-
-
-class Activation_ReLU:
-    def forward(self, inputs):
-        self.output = np.maximum(0, inputs)
-        self.inputs = inputs
-    
-    def backward(self, dvalues):
-        self.dinputs = dvalues.copy()
-        self.dinputs[self.inputs <= 0] = 0
-
-
-class Activation_Softmax:
-    def forward(self, inputs):
-        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
-        self.output = probabilities
-        self.inputs = inputs
-    
-    def backward(self, dvalues):
-        self.dinputs = np.empty_like(dvalues)
-        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
-            single_output = single_output.reshape(-1, 1)
-            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
-            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
-
-
-class Loss:
-    def calculate(self, output, y): # y is the target values
-        sample_losses = self.forward(output, y) 
-        data_loss = np.mean(sample_losses)
-        return data_loss
-
-
-class Loss_CategoricalCrossentropy(Loss):
-    def forward(self, y_pred, y_true):
-        samples = len(y_pred)
-        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7) # clip both sides to prevent division by 0
-        
-        if len(y_true.shape) == 1: # if targets are in a single dimension
-            correct_confidences = y_pred_clipped[range(samples), y_true]
-        
-        elif len(y_true.shape) == 2: # if targets are in a one-hot encoded format
-            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
-        
-        negative_log_likelihoods = -np.log(correct_confidences)
-        return negative_log_likelihoods
-
-    def backward(self, dvalues, y_true):
-        samples = len(dvalues)
-        labels = len(dvalues[0])
-        if len(y_true.shape) == 1:
-            y_true = np.eye(labels)[y_true]
-        self.dinputs = -y_true / dvalues
-        self.dinputs = self.dinputs / samples
-
-
-class Activation_Softmax_Loss_CategoricalCrossentropy():
-    def __init__(self):
-        self.activation = Activation_Softmax()
-        self.loss = Loss_CategoricalCrossentropy()
-    
-    def forward(self, inputs, y_true):
-        self.activation.forward(inputs)
-        self.output = self.activation.output
-        return self.loss.calculate(self.output, y_true)
-    
-    def backward(self, dvalues, y_true):
-        samples = len(dvalues)
-        if len(y_true.shape) == 2:
-            y_true = np.argmax(y_true, axis=1)
-        self.dinputs = dvalues.copy()
-        self.dinputs[range(samples), y_true] -= 1
-        self.dinputs = self.dinputs / samples
-
-
-class Optimizer_SGD:
-    global learning_rate
-    def __init__(self, learning_rate):
-        self.learning_rate = learning_rate
-    def update_params(self, layer):
-        layer.weights += -self.learning_rate * layer.dweights
-        layer.biases += -self.learning_rate * layer.dbiases
-    def update_learning_rate(self, new_learning_rate):
-        self.learning_rate = new_learning_rate
-
-
 
 
 # load_configurations is the function responsible for loading the configurations from the configuration file
@@ -160,6 +54,7 @@ def load_configurarions():
     global size_hidden_layers
     global learning_rate
     global number_of_epochs
+    global rate_of_decrease
 
     with open('Config\initial_config.json', 'r') as file:
         configurations = json.load(file)
@@ -169,6 +64,7 @@ def load_configurarions():
     size_hidden_layers = configurations['size_hidden_layers']
     number_of_hidden_layers = configurations['number_of_hidden_layers']
     learning_rate = configurations['learning_rate']
+    rate_of_decrease = configurations['rate_of_decrease']
     number_of_epochs = configurations['number_of_epochs']
 
 
@@ -305,13 +201,13 @@ def initialize_objects():
     global loss_activation
     global optimizer
 
-    hidden_layer1 = layer_dense(number_of_inputs, size_hidden_layers, weights_layer1, biases_layer1) 
-    hidden_layer2 = layer_dense(size_hidden_layers, size_hidden_layers, weights_layer2, biases_layer2) 
-    hidden_layer3 = layer_dense(size_hidden_layers, number_of_outputs, weights_layer3, biases_layer3)
+    hidden_layer1 = classes.layer_dense(number_of_inputs, size_hidden_layers, weights_layer1, biases_layer1) 
+    hidden_layer2 = classes.layer_dense(size_hidden_layers, size_hidden_layers, weights_layer2, biases_layer2) 
+    hidden_layer3 = classes.layer_dense(size_hidden_layers, number_of_outputs, weights_layer3, biases_layer3)
 
-    activation1 = Activation_ReLU()
-    loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
-    optimizer = Optimizer_SGD(learning_rate)
+    activation1 = classes.Activation_ReLU()
+    loss_activation = classes.Activation_Softmax_Loss_CategoricalCrossentropy()
+    optimizer = classes.Optimizer_SGD(learning_rate)
 
 
 
@@ -339,6 +235,8 @@ def train_data():
     global loss_activation
     global optimizer
     global loss
+    global best_loss
+    global rate_of_decrease
 
     last_loss = float('inf')
     for epoch in range(number_of_epochs):
@@ -383,7 +281,7 @@ def train_data():
 
         if(epoch % 250 == 0):
             if loss > last_loss:
-                optimizer.update_learning_rate(optimizer.learning_rate * 0.66)
+                optimizer.update_learning_rate(optimizer.learning_rate * rate_of_decrease)
             else:
                 last_loss = loss
 
